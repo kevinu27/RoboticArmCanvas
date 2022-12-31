4efdceebdd99b16203215c7310fd2ba9dc2bfbc0
Kevin Santana Medina <Kevin.Santana.Medina@es.ey.com>
2022-12-31 01:20:49 +0100

    set coordinates,steps, clean, path

--- main.js
+++ main.js
@@ -5,6 +5,41 @@ const lenghtTwo = 250
 const radioCircle = lenghtOne + lenghtTwo
 var teta2Grados
 var teta1Grados
+var path = [{x:0, y:0}] 
+var clickado = false
+let pathCheckbox 
+let motorSimulation = false
+let framesCounter=0
+let initialPosForSimulation = true
+let prevFrameCounter
+let xCoordinate 
+let yCoordinate
+let steps
+
+document.getElementById("path").onclick = function() {checkingboxFunction()};
+function checkingboxFunction() {
+    pathCheckbox = document.getElementById('path').checked
+  }
+  document.getElementById("clean").onclick = function() {checkingboxFunction2()};
+function checkingboxFunction2() {
+    path = [{x:0, y:0}] 
+  }
+
+  document.getElementById("coordinatesButton").addEventListener("click", setCoordinates);
+
+function setCoordinates() {
+    xCoordinate =  document.getElementById("xCoordinate").value;
+    yCoordinate = document.getElementById("yCoordinate").value;
+    steps = document.getElementById("steps").value;
+
+    console.log("xCoordinate: ", xCoordinate)
+    console.log("yCoordinate: ", yCoordinate)
+    console.log("steps: ", steps)
+    motorSimulation = !motorSimulation 
+    prevFrameCounter = framesCounter
+
+}
+
 
 const game = {
     canvasDom: undefined,
@@ -44,13 +79,16 @@ const game = {
     setContext() {
         this.canvasDOM = document.querySelector('#myCanvas')
         this.ctx = this.canvasDOM.getContext('2d')
+        // this.ctx.setTransform(2, 0, 0, 2, this.canvasSize.w / 2, this.canvasSize.h / 2); // zooms in by 2 with origin at center
+        // this.ctx.setTransform(0.5, 0, 0, 0.5, this.canvasSize.w / 2, this.canvasSize.h/ 2); // zooms out by 2 with origin at center
     },
     setDimensions() {
         this.canvasSize.w = window.innerWidth
         this.canvasSize.h = window.innerHeight
         // console.log('window.innerHeight', window.innerHeight)
-        this.canvasDOM.setAttribute('width', this.canvasSize.w/1.5)
-        this.canvasDOM.setAttribute('height', this.canvasSize.h/2)
+        this.canvasDOM.setAttribute('width', this.canvasSize.w/2)
+        this.canvasDOM.setAttribute('height', this.canvasSize.h)
+        
     },
 
     setEventListeners() {
@@ -66,16 +104,31 @@ const game = {
             that.x =event.clientX - 8
             that.y = event.clientY - 8
         }
+        canvas.addEventListener('mousedown', function(e) {
+            getCursorClicked(canvas, e)
+            })
+            function getCursorClicked(canvas, event) {
+             clickado = !clickado
+            //  console.log("cliackdo:", clickado) 
+            }
     },
 
     start(){
         this.createLine()
         
         setInterval(()=> {
-            this.clearAll()
-            this.calculation()
-            this.drawAll()
-            // this.calculation()
+            framesCounter > 50000 ? framesCounter = 0 : framesCounter++
+            if(!motorSimulation){
+                this.clearAll()
+                this.calculation()
+                this.drawAll()
+        }
+            if(motorSimulation){
+                this.clearAll()
+                this.calculationMotorSimulation()
+                this.drawAllMotorSimulation()
+        }
+
         }, 1000/this.FPS)
     },
     
@@ -103,14 +156,23 @@ const game = {
         this.lineThree.draw()
         this.drawCircle()
         this.drawText( this.x,  this.y)
-        
+       
+        //punto del raton
         this.ctx.beginPath();
         this.ctx.arc(this.x, this.y, 5, 0, Math.PI*2);
         this.ctx.fillStyle = "#000000";
         this.ctx.fill();
         this.ctx.closePath();
 
-
+        //path
+        for(let i = 0; i<=path.length-1; i++){
+        this.ctx.beginPath();
+        this.ctx.arc(path[i].x, path[i].y, 5, 0, Math.PI*2);
+        this.ctx.fillStyle = "#00ff00";
+        this.ctx.fill();
+        this.ctx.closePath();
+        this.ctx.fillStyle = "#000000";
+        }
 
 
     }, 
@@ -131,7 +193,7 @@ const game = {
     },
 
     calculation(){
-        console.log("calculo")
+        // console.log("calculo")
         // teorema del coseno
         // (lenghtOne*lenghtOne) + (lenghtTwo*lenghtTwo) - 2*lenghtOne*lenghtTwo*Math.cos(Beta) = this.x + this.y
         const beta= Math.acos(((lenghtOne*lenghtOne) + (lenghtTwo*lenghtTwo) - (this.x*this.x) - (this.y*this.y))/(2*lenghtOne*lenghtTwo))
@@ -167,9 +229,74 @@ const game = {
 
         this.lineTwo.endX = Math.cos(angleTwoConst*(Math.PI / 180))*lenghtTwo + Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
         this.lineTwo.endY = Math.sin(angleTwoConst*(Math.PI / 180)) *lenghtTwo + Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
+       
+        if(this.lineTwo.endX != path[path.length-1].x && clickado=== false && pathCheckbox){
+        path.push({x:this.lineTwo.endX , y:this.lineTwo.endY})
+        // console.log("path", path)
+        }
+        
 
         this.lineThree.endX= Math.cos(angleTwoConst*(Math.PI / 180))*lenghtTwo + Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
         this.lineThree.endY= Math.sin(angleTwoConst*(Math.PI / 180)) *lenghtTwo + Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
 //-------------------       
+    },
+    calculationMotorSimulation(){
+        if(initialPosForSimulation){
+        this.lineOne.originX = 0  
+        this.lineOne.originY = 0
+        this.lineOne.endX = 226
+        this.lineOne.endY = 105 
+        this.lineOne.draw()
+        this.lineTwo.originX = 226  
+        this.lineTwo.originY = 105
+        this.lineTwo.endX = 1
+        this.lineTwo.endY = 220
+        this.lineTwo.draw()
+        initialPosForSimulation = false
+        setTimeout(function() {
+          }, 3000);
+    }else{
+        const beta= Math.acos(((lenghtOne*lenghtOne) + (lenghtTwo*lenghtTwo) - (xCoordinate*xCoordinate) - (yCoordinate*yCoordinate))/(2*lenghtOne*lenghtTwo))
+        const betaGrado =beta*180/Math.PI
+
+        const alfa =Math.acos(((xCoordinate*xCoordinate) + (yCoordinate*yCoordinate) + (lenghtOne*lenghtOne) - (lenghtTwo*lenghtTwo)) / (2*lenghtOne*(Math.sqrt(((xCoordinate*xCoordinate) + (yCoordinate*yCoordinate)))) ) )
+
+        const lambda = Math.atan(yCoordinate/xCoordinate)
+        const teta1= lambda - alfa
+         teta1Grados = parseInt(teta1*180/Math.PI)
+        const teta2 = Math.PI - beta
+         teta2Grados = parseInt(teta2*180/Math.PI)
+        angleOneConst =teta1Grados
+        angleTwoConst =teta2Grados + teta1Grados
+        this.initialXOne=  Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
+        this.initialYOne= Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
+        this.endXTwo=  Math.cos(angleTwoConst*(Math.PI / 180))*lenghtTwo + Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
+        this.endYTwo= Math.sin(angleTwoConst*(Math.PI / 180)) *lenghtTwo + Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
+//----------
+        this.lineOne.endX =Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
+        this.lineOne.endY =Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
+
+        this.lineTwo.originX = Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
+        this.lineTwo.originY = Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
+
+        this.lineTwo.endX = Math.cos(angleTwoConst*(Math.PI / 180))*lenghtTwo + Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
+        this.lineTwo.endY = Math.sin(angleTwoConst*(Math.PI / 180)) *lenghtTwo + Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
+       
+        // if(this.lineTwo.endX != path[path.length-1].x && clickado=== false && pathCheckbox){
+        // path.push({x:this.lineTwo.endX , y:this.lineTwo.endY})
+        // // console.log("path", path)
+        // }
+        
+
+        this.lineThree.endX= Math.cos(angleTwoConst*(Math.PI / 180))*lenghtTwo + Math.cos(angleOneConst*(Math.PI / 180))*lenghtOne
+        this.lineThree.endY= Math.sin(angleTwoConst*(Math.PI / 180)) *lenghtTwo + Math.sin(angleOneConst*(Math.PI / 180)) *lenghtOne
+    }
+    },
+    drawAllMotorSimulation(){
+        this.drawCircle()
+        this.drawText( this.x,  this.y)
+        this.lineOne.draw()
+        this.lineTwo.draw()
+
     }
 }
